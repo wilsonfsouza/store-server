57c8c86c605af3899281ea0d29b70764ec9e09b5
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-25 21:32:14 -0800

    feat(products): added products repository with method to create, findByName, findAllById, and updateQuantity.

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,65 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProduct = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const findProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const findProducts = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    const updatedProducts = findProducts.map(product => {
+      const productToBeUpdated = products.find(item => item.id === product.id);
+
+      return {
+        ...product,
+        quantity: productToBeUpdated
+          ? productToBeUpdated.quantity
+          : product.quantity,
+      };
+    });
+
+    // await this.ormRepository.save(updatedProducts);
+
+    return updatedProducts;
+    // return this.ormRepository.save(products);
   }
 }
 
